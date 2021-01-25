da3042fbd263ae0363932eb84d3f848c61307a91
IngaPanczyk <ingawaaa@gmail.com>
2021-01-25 21:40:27 +0100

    utworzenie struktury projektu

---
+++ src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
@@ -0,0 +1,6 @@
+package com.kodilla.testing2.config;
+
+public class WebDriverConfig {
+    public final static String FIREFOX = "FIREFOX_DRIVER"
+    public final static String CHROME = "CHROME_DRIVER"
+}
